48fa3d4a38af7f0c178ced9d7794986deea95e78
Karim Mammadli <kmammadl@purdue.edu>
2022-12-29 20:46:12 -0600

    Register centered & minor fixes

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "./gif.css";
 
 export default function Register() {
     // const [firstName, setfirstName] = useState("");
@@ -36,16 +35,13 @@ export default function Register() {
     //   };
 
     return (
-
-        
       <>
-        
-        <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-          <div className="w-full max-w-md space-y-8">
+        <div className="flex align-middle min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8" >
+          <div className="w-full mt-36 max-w-md space-y-8">
             <div>
 
               <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-                Register to GamBid
+                Welcome to GamBid
               </h2>
 
             </div>
